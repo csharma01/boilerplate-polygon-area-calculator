281e3f7390680af0c1a0c6aca827a443662ed79e
Chakshu Sharma <41478169+csharma01@users.noreply.github.com>
2023-03-15 12:28:23 +0000

    Initial commit

--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,53 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** 0.5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return "Too big for picture."
+        else:
+            picture = ""
+            for i in range(self.height):
+                picture += "*"*self.width + "\n"
+            return picture
+
+    def get_amount_inside(self, shape):
+        if not isinstance(shape, Rectangle):
+            raise TypeError("The argument must be a Rectangle.")
+        if shape.width > self.width or shape.height > self.height:
+            return 0
+        else:
+            width_ratio = self.width // shape.width
+            height_ratio = self.height // shape.height
+            return width_ratio * height_ratio
+
+    def __str__(self):
+        return f"Rectangle(width={self.width}, height={self.height})"
 
 
+class Square(Rectangle):
+    def __init__(self, side):
+        super().__init__(side, side)
 
+    def set_side(self, side):
+        self.width = side
+        self.height = side
 
-class Square:
+    def __str__(self):
+        return f"Square(side={self.width})"
